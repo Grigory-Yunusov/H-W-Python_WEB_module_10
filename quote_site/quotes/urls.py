5f97558e101120c8577c780c04520bb50287c43a
Grigory-Yunusov <grigoriyzawr@gmail.com>
2024-03-15 08:26:34 +0300

    Exception Value: 'quotes' is not a registered namespace

--- quote_site/quotes/urls.py
+++ quote_site/quotes/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = "quotes"
 
 urlpatterns = [
-    path("", views.home, name="home"), # quotes://home
+    path("", views.home, name="home"), # quotes:home
+    path("author", views.author_detail, name="author_detail"), 
+    path("add_author", views.add_author, name="add_author"), 
+    path("add_quote", views.add_quote, name="add_quote"), 
 ]
