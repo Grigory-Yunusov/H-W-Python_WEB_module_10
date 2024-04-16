359a3d385a0a91dbb51ff9f75784f4379de5198c
Grigory-Yunusov <grigoriyzawr@gmail.com>
2024-04-16 12:52:50 +0300

    add_Forgot_Password

--- quote_site/quote_site/settings.py
+++ quote_site/quote_site/settings.py
@@ -27,6 +27,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.meta.ua'
+EMAIL_PORT = 465
+EMAIL_STARTTLS = False
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+EMAIL_HOST_USER = 'Project_FastAPI@meta.ua'
+EMAIL_HOST_PASSWORD = 'DreamWorks_1992//'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
 
 # Application definition
 
--- quote_site/users/views.py
+++ quote_site/users/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import RegisterForm, LoginForm
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 
 
 def signupuser(request):
@@ -39,3 +42,16 @@ def loginuser(request):
 def logoutuser(request):
     logout(request)
     return redirect(to='quotes:home')
+
+# @login_required
+# def profile(request):
+#     return render(request, 'users/profile.html')
+
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'users/password_reset.html'
+    email_template_name = 'users/password_reset_email.html'
+    html_email_template_name = 'users/password_reset_email.html'
+    success_url = reverse_lazy('users:password_reset_done')
+    success_message = "An email with instructions to reset your password has been sent to %(email)s."
+    subject_template_name = 'users/password_reset_subject.txt'
